abb90fb85a170ebd7200157e43342bc886234c8a
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-02 23:00:54 -0500

    updated

--- math1.clj
+++ math1.clj
@@ -3,7 +3,8 @@
 ;;; Leif Anderson November 28, 2016
 
 ;;; quatratic equation solver
-
+;;; naive impl for testing
+;;;
 (defn disciminant [a b c]
 	(- (* b b) (* 4 a c)))
 
@@ -14,8 +15,19 @@
 
 (defn find-one-solution [a b]
 	(list (/ (- b) (* 2 a))))
-	
-(defn solve-quadratic-equation [a b c] ())
+
+(defn find-two-solutions [a b c]
+	(list (/ (+ (- b) (Math.sqrt (- (* b b) (* 4 a c))))
+					(* 2 a))
+	(list (/ (- (- b) (Math.sqrt (- (* b b) (* 4 a c))))
+					(* 2 a))))
+
+(defn solve-quadratic-equation [a b c]
+	(cond ((zero? (- (* b b) (* 4 a c)))
+					(find-one-solution a b))
+				((positive? (- (* b b) (* 4 a c)))
+					(find-two-solutions a b c))
+				(else '() )))
 
 (defn solve-equation [a b c]
 	(if (zero? a)
